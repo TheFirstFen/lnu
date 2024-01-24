c98e0d77cd0f6ca6bb998190efcdecdb48f74970
Samuel Berg <sb224sc@student.lnu.se>
2024-01-24 10:27:30 +0100

    not fuctioning kruskals

--- year2/code/1dt907/lab3/Main3.java
+++ year2/code/1dt907/lab3/Main3.java
@@ -1,14 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import alogrithms.graphs.Kruskal;
+import alogrithms.graphs.Edge;
 
 public class Main3 {
     public static void main(String[] args) {
-        Kruskal kruskal = new Kruskal(6, 6);
+        List<Edge<Integer>> edges = new ArrayList<>();
 
-        kruskal.addEdge(0, 2, 3.2);
-        kruskal.addEdge(1, 2, 1.2);
-        kruskal.addEdge(2, 3, 4.3);
-        kruskal.addEdge(3, 4, 2.2);
+        edges.add(new Edge<Integer>(0, 2, 3.2));
+        edges.add(new Edge<Integer>(1, 2, 1.2));
+        edges.add(new Edge<Integer>(2, 3, 4.3));
+        edges.add(new Edge<Integer>(3, 4, 2.2));
 
-        kruskal.runKurskal();
+        edges.add(new Edge<Integer>(5, 6, 2.5));
+        edges.add(new Edge<Integer>(7, 6, 1.8));
+        edges.add(new Edge<Integer>(6, 8, 3.2));
+        edges.add(new Edge<Integer>(8, 9, 1.5));
+
+        Kruskal<Integer> kruskal = new Kruskal<Integer>();
+
+        for (List<Edge<Integer>> tree : kruskal.runKurskal(edges, edges.size() * 2)) {
+            System.out.println("Tree:");
+            for (Edge<Integer> edge : tree) {
+                System.out.println(edge);
+            }
+        }
+
+        // kruskal.addEdge(0, 2, 3.2);
+        // kruskal.addEdge(1, 2, 1.2);
+        // kruskal.addEdge(2, 3, 4.3);
+        // kruskal.addEdge(3, 4, 2.2);
+
     }
 }
--- year2/code/1dt907/lab3/alogrithms/graphs/Kruskal.java
+++ year2/code/1dt907/lab3/alogrithms/graphs/Kruskal.java
@@ -1,33 +1,72 @@
 package alogrithms.graphs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import alogrithms.*;
 
-public class Kruskal {
-    private Heap<Integer> heap;
-    private PCWQUnionFind uf;
+public class Kruskal<T> {
+    // private Heap<Integer> heap;
+    // private PCWQUnionFind uf;
 
-    public Kruskal(int heapCap, int vertices) {
-        this.heap = new Heap<Integer>(heapCap);
-        this.uf = new PCWQUnionFind(vertices);
-    }
+    // public Kruskal(int heapCap, int vertices) {
+    // this.heap = new Heap<Integer>(heapCap);
+    // this.uf = new PCWQUnionFind(vertices);
+    // }
 
-    public void addEdge(int v, int w, double weight) {
-        Edge<Integer> edge = new Edge<Integer>(v, w, weight);
-        Edge<Integer> rEdge = new Edge<Integer>(w, v, weight);
-        heap.insert(edge);
-        heap.insert(rEdge);
-    }
+    // public void addEdge(int v, int w, double weight) {
+    // Edge<Integer> edge = new Edge<Integer>(v, w, weight);
+    // Edge<Integer> rEdge = new Edge<Integer>(w, v, weight);
+    // heap.insert(edge);
+    // heap.insert(rEdge);
+    // }
 
-    public void runKurskal() {
-        while (!heap.isEmpty() && uf.getCount() > 1) {
-            Edge<Integer> edge = heap.poll();
-            int srcRoot = uf.find(edge.v1);
-            int destRoot = uf.find(edge.v2);
+    // public void runKurskal() {
+    // while (!heap.isEmpty() && uf.getCount() > 1) {
+    // Edge<Integer> edge = heap.poll();
+    // int srcRoot = uf.find(edge.v1);
+    // int destRoot = uf.find(edge.v2);
 
-            if (srcRoot != destRoot) {
-                System.out.println("Edge: " + edge);
-                uf.union(edge.v1, edge.v2);
+    // if (srcRoot != destRoot) {
+    // System.out.println("Edge: " + edge);
+    // uf.union(edge.v1, edge.v2);
+    // }
+    // }
+    // }
+
+    public List<List<Edge<T>>> runKurskal(List<Edge<T>> edges, int numVertices) {
+        Heap<T> minHeap = new Heap<>(numVertices);
+
+        for (Edge<T> edge : edges) {
+            minHeap.insert(edge);
+        }
+
+        PCWQUnionFind unionFind = new PCWQUnionFind(numVertices);
+
+        List<List<Edge<T>>> minimalSpanningForest = new ArrayList<>();
+
+        while (!minHeap.isEmpty() && minimalSpanningForest.size() < numVertices - 1) {
+            Edge<T> edge = minHeap.poll();
+
+            int root1 = unionFind.find((int) edge.v1);
+            int root2 = unionFind.find((int) edge.v2);
+
+            if (root1 != root2) {
+                List<Edge<T>> newTree = new ArrayList<>();
+                newTree.add(edge);
+                minimalSpanningForest.add(newTree);
+
+                unionFind.union(root1, root2);
+            } else {
+                for (List<Edge<T>> tree : minimalSpanningForest) {
+                    if (unionFind.find((int) tree.get(0).v1) == root1) {
+                        tree.add(edge);
+                        break;
+                    }
+                }
             }
         }
+
+        return minimalSpanningForest;
     }
 }
