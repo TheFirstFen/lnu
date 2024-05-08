4c0ec40c63d826d85c15352afe9b9485e1aa715a
Samuel Berg <sb224sc@student.lnu.se>
2024-05-08 12:51:59 +0200

    started working on 2dt901 lab3 t1 & t2

--- y2/code/2dt901/labs/lab3/c_test/HelloWorld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
-
-int main()
-{
-	stdio_init_all();
-	printf("Hello, World!\n");
-	return 0;
-}
---
+++ y2/code/2dt901/labs/lab3/pico_test/c_test/sdklink.c
@@ -0,0 +1,11 @@
+#include "hardware/gpio.h"
+
+void link_gpio_set_dir(int pin, int dir)
+{
+    gpio_set_dir(pin, dir);
+}
+
+void link_gpio_put(int pin, int val)
+{
+    gpio_put(pin, val);
+}
