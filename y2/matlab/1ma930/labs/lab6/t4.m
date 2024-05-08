5a4bd8531365f3ff667c9a3be21ffec80ff33f0f
Samuel Berg <sb224sc@student.lnu.se>
2024-05-08 15:00:21 +0200

    1ma930 lab6

--- y2/matlab/1ma930/labs/lab6/t4.m
+++ y2/matlab/1ma930/labs/lab6/t4.m
@@ -1,2 +1,17 @@
+t3
 clc
+
+K = 8;
+
+err = zeros(1,K);
+
+for k = 1:K
+    n = 10^k;
+    h = 1/n;
+    w = y0;
+    for i = 1:n
+        w = w+ h/2*(f((i-1)/n,w) + f((i-1)/n+h,w+h*f((i-1)/n,w)));
+    end
+
+end
 
