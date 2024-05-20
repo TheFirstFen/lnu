17d402cdcd23c9f535f40bc0a6c925b97f4d78c7
Samuel Berg <sb224sc@student.lnu.se>
2024-05-20 14:04:43 +0200

    2dt901 lab4 t2 fix

--- y2/code/2dt901/labs/lab4/t2/main.c
+++ y2/code/2dt901/labs/lab4/t2/main.c
@@ -15,8 +15,8 @@
 volatile int counter = 0;
 
 void reset();
-void inc(uint gpio, uint32_t events);
-void dec(uint gpio, uint32_t events);
+void inc();
+void dec();
 
 int main() {
     stdio_init_all();
@@ -39,13 +39,13 @@ int main() {
 
     gpio_init(INC);
     gpio_set_dir(INC, GPIO_IN);
-    gpio_set_irq_enabled(INC, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true);
+    gpio_set_irq_enabled(INC, GPIO_IRQ_EDGE_RISE, true);
     irq_set_enabled(IO_IRQ_BANK0, true);
     gpio_add_raw_irq_handler(INC, &inc);
 
     gpio_init(DEC);
     gpio_set_dir(DEC, GPIO_IN);
-    gpio_set_irq_enabled(DEC, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true);
+    gpio_set_irq_enabled(DEC, GPIO_IRQ_EDGE_RISE, true);
     irq_set_enabled(IO_IRQ_BANK0, true);
     gpio_add_raw_irq_handler(DEC, &dec);
 
@@ -142,18 +142,18 @@ int main() {
     return 0;
 }
 
-void inc(uint gpio, uint32_t events) {
-    if (gpio_get_irq_event_mask(INC) & GPIO_IRQ_EDGE_FALL) {
-        gpio_acknowledge_irq(INC, GPIO_IRQ_EDGE_FALL);
+void inc() {
+    if (gpio_get_irq_event_mask(INC) & GPIO_IRQ_EDGE_RISE) {
+        gpio_acknowledge_irq(INC, GPIO_IRQ_EDGE_RISE);
         if (counter < 15) {
             counter++;
         }
     }
 }
 
-void dec(uint gpio, uint32_t events) {
-    if (gpio_get_irq_event_mask(DEC) & GPIO_IRQ_EDGE_FALL) {
-        gpio_acknowledge_irq(DEC, GPIO_IRQ_EDGE_FALL);
+void dec() {
+    if (gpio_get_irq_event_mask(DEC) & GPIO_IRQ_EDGE_RISE) {
+        gpio_acknowledge_irq(DEC, GPIO_IRQ_EDGE_RISE);
         if (counter > 0) {
             counter--;
         }
