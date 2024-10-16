ac684765393996427a4ca5b6fa30fbf4fa0812d1
Samuel Berg <sb224sc@student.lnu.se>
2024-10-16 20:26:38 +0200

    before test compile

--- y3/code/2dt903/labs/lab2/main.c
+++ y3/code/2dt903/labs/lab2/main.c
@@ -2,59 +2,46 @@
 #include "pico/stdlib.h"
 #include "FreeRTOS.h"
 #include "task.h"
-#include "hardware/adc.h"
+#include "DHT.h"
 
-const uint BUTTON_PIN = 26;
-const uint LED_PIN = 27;
-const uint DHT_SENSOR_PIN = 28;
+define BTN_PIN 0;
+define LED_PIN 1;
+define DHT_PIN 16;
 
-int previousButtonValue = 1;
+DHT dht;
 
-void sensor_task(void *pvParameters) { // TODO: 
-    while (true) {
-        int lightValue = adc_read();
-        double lightVoltage = ((double)lightValue / 4095.0) * 3.3;
+void temp_task(void *pvParameters) {
+    for (;;) {
+        dht.measure();
+        float temp = dht.getTemperatureFloat();
+        float hum = dht.getHumidityFloat();
 
-        for (int i = 9; i > 0; i--) {
-            avg_list[i] = avg_list[i-1];
+        if (isnan(temp) || isnan(hum)) {
+            printf("Failed to read DHT sensor values!\n");
+        } else {
+            printf("Temperature: %.1f °C, Humidity: %.1f%%\n", temp, hum);
         }
-        avg_list[0] = lightVoltage;
-        double voltageSum = 0;
-        for (int i = 0; i < 10; i++) {
-            voltageSum += avg_list[i];
-        }
-        lightAverage = voltageSum / 10.0;
 
-        if (lightAverage > lightOnThreshold && previuosLightAverage < lightOnThreshold) {
-            gpio_put(LED_PIN,1);
-        } else if (lightAverage < lightOffThreshold && previuosLightAverage > lightOffThreshold) {
-            gpio_put(LED_PIN,0);
-        }
-        previuosLightAverage = lightAverage;
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
-uint64_t getCurrentTimeTick() {
-    TickType_t ticks = xTaskGetTickCount();
-    uint64_t In_ticks = (uint64_t)ticks;
-    return In_ticks;
-}
 
-void btn_cb(void *pvParameters) { // TODO:
-    while (true) {
-        uint64_t timeStart = getCurrentTimeTick();
-        if (gpio_get(BUTTON_PIN) == 0 && previousButtonValue == 1) {
-            gpio_put(LED_PIN,!gpio_get(LED_PIN));
-            uint64_t timeEnd = getCurrentTimeTick();
-            printf("Current time in ticks: %llu\n", timeEnd-timeStart);
+void btn_task(void *pvParameters) {
+    bool ledState = false;
+
+    while (1) {
+        if (gpio_get(BTN_PIN)) {
+            ledState != ledState;
+            gpio_put(LED_PIN, ledState);
+            vTaskDelay(pdMS_TO_TICKS(500));
         }
-        previousButtonValue = gpio_get(BUTTON_PIN);
-        vTaskDelay(pdMS_TO_TICKS(1));
-
+        vTaskDelay(pdMS_TO_TICKS(50));
     }
 }
 
 int main() {
     stdio_init_all();
+    
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
@@ -62,15 +49,14 @@ int main() {
     gpio_set_dir(BUTTON_PIN, GPIO_IN);
     gpio_pull_up(BUTTON_PIN);
 
-    adc_init();
-    adc_gpio_init(DHT_SENSOR_PIN);
-    adc_select_input(2);
+    gpio_init(DHT_PIN);
+    dht = DHT11(DHT_PIN);
 
-    xTaskCreate(sensor_task, "LED_Task", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
-    xTaskCreate(btn_cb, "ButtonTask", configMINIMAL_STACK_SIZE, NULL, 10, NULL);
+    xTaskCreate(temp_task, "tempTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xTaskCreate(btn_task, "btnTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
-    while (1) {}
+    while (1);
     return 0;
 }
