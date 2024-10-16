8e5a0e714cf948d157c69f09d6427229edbab909
Samuel <149721534+TheFirstFen@users.noreply.github.com>
2024-10-16 16:46:30 +0200

    Update example.c

--- y3/code/2dt903/labs/lab2/example.c
+++ y3/code/2dt903/labs/lab2/example.c
@@ -1,120 +1,81 @@
-import network
-import time
-import machine
-import umqtt
-import ubinascii
-import machine
-import dht
-import json 
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "hardware/adc.h"
 
-ssid = 'fen'
-password = '3q1t6ibj'
-topic = "Samuel/RPIPico"
-btn = machine.Pin(0, machine.Pin.IN)
-led = machine.Pin(1, machine.Pin.OUT)
-dht = dht.DHT11(machine.Pin(16))
+const uint BUTTON_PIN = 26;
+const uint LED_PIN = 27;
+const uint LIGHT_SENSOR_PIN = 28;
 
-mqtt_c = None
+const double lightOnThreshold = 2.6;
+const double lightOffThreshold = 2.5;
+double previuosLightAverage = 0;
+double lightAverage = 0;
+double avg_list[10] = {0};
+int previousButtonValue = 1;
 
-def conn_wifi():
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    
-    if not wlan.isconnected():
-        print(f"Connecting to Wi-Fi: {ssid}")
-        wlan.connect(ssid, password)
-        
-        while not wlan.isconnected():
-            time.sleep(1)
-            print(" .", end="")
-    
-    print("\nConnected to Wi-Fi")
-    print(wlan.ifconfig()) 
+void sensor_task(void *pvParameters) {
+    while (true) {
+        int lightValue = adc_read();
+        double lightVoltage = ((double)lightValue / 4095.0) * 3.3;
 
-def conn_mqtt():
-    global mqtt_c
-    try:
-        mqtt_c = umqtt.MQTTClient(client_id=ubinascii.hexlify(machine.unique_id()).decode(), server="broker.emqx.io", port=1883, user="", password="")
-        mqtt_c.connect()
-        print("Connected to MQTT")
-    except Exception as e:
-        print(f"Failed to connect to MQTT: {e}")
-        mqtt_c = None
+        for (int i = 9; i > 0; i--) {
+            avg_list[i] = avg_list[i-1];
+        }
+        avg_list[0] = lightVoltage;
+        double voltageSum = 0;
+        for (int i = 0; i < 10; i++) {
+            voltageSum += avg_list[i];
+        }
+        lightAverage = voltageSum / 10.0;
 
-def p_msg(mqtt_c, topic, message):
-    
-    if mqtt_c:
-        try:
-            mqtt_c.publish(topic, message)
-            print(f"Message published to {topic}: {message}")
-        except Exception as e:
-            print(f"Failed to publish message: {e}")
-    else:
-        print("MQTT client not connected.")
-    led.off()
+        if (lightAverage > lightOnThreshold && previuosLightAverage < lightOnThreshold) {
+            gpio_put(LED_PIN,1);
+        } else if (lightAverage < lightOffThreshold && previuosLightAverage > lightOffThreshold) {
+            gpio_put(LED_PIN,0);
+        }
+        previuosLightAverage = lightAverage;
+    }
+}
+uint64_t getCurrentTimeTick() {
+    TickType_t ticks = xTaskGetTickCount();
+    uint64_t In_ticks = (uint64_t)ticks;
+    return In_ticks;
+}
 
-def msg_cb(topic, msg):
-    print(f"Received message: {msg.decode()} from topic: {topic.decode()} \n")
-    if msg.decode() == "Data comming..." or msg.decode() == "Btn data comming...":
-        temp()
-        p_msg(mqtt_c, topic, "Data ending...")
-    else:
-        led.on()
+void Button_Task(void *pvParameters) {
+    while (true) {
+        uint64_t timeStart = getCurrentTimeTick();
+        if (gpio_get(BUTTON_PIN) == 0 && previousButtonValue == 1) {
+            gpio_put(LED_PIN,!gpio_get(LED_PIN));
+            uint64_t timeEnd = getCurrentTimeTick();
+            printf("Current time in ticks: %llu\n", timeEnd-timeStart);
+        }
+        previousButtonValue = gpio_get(BUTTON_PIN);
+        vTaskDelay(pdMS_TO_TICKS(1));
 
-def sub_topic(mqtt_c, topic):
-    if mqtt_c:
-        mqtt_c.set_callback(msg_cb)
-        try:
-            mqtt_c.subscribe(topic)
-            print(f"Subscribed to {topic}")
-        except Exception as e:
-            print(f"Failed to subscribe to topic: {e}")
-    else:
-        print("MQTT client not connected.")
+    }
+}
 
-def button_pressed():
-    if btn.value() == 1:
-        time.sleep(0.05)
-        if btn.value() == 1:
-            return True
-    return False
+int main() {
+    stdio_init_all();
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
 
-def temp():
-    dht.measure()
-    temp = dht.temperature() 
-    hum = dht.humidity()
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN);
 
-    payload = json.dumps({"temp": temp, "hum": hum})
-    p_msg(mqtt_c, topic, payload)
+    adc_init();
+    adc_gpio_init(LIGHT_SENSOR_PIN);
+    adc_select_input(2);
 
-def main():
-    conn_wifi()
-    conn_mqtt()
-    
-    if mqtt_c:
-        sub_topic(mqtt_c, topic)
-        
-        try:
-            elapsed_time = time.time()
-            
-            while True:
-                mqtt_c.check_msg()
-                
-                if time.time() - elapsed_time >= 300:
-                    p_msg(c, topic, "Data comming...")
-                    elapsed_time = time.time()
-                
-                if button_pressed():
-                    led.on()
-                    p_msg(mqtt_c, topic, "Btn data comming...")
-                    time.sleep(0.5)
-                led.off()
-                
-        except KeyboardInterrupt:
-            print("Disconnecting from MQTT...")
-            if mqtt_c:
-                mqtt_c.disconnect()
+    xTaskCreate(sensor_task, "LED_Task", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xTaskCreate(Button_Task, "ButtonTask", configMINIMAL_STACK_SIZE, NULL, 10, NULL);
 
-main()
+    vTaskStartScheduler();
 
-
+    while (1) {}
+    return 0;
+}
