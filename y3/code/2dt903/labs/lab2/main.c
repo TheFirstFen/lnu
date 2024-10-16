58e930a10d28aa23bae64bda3781709761e781ad
Samuel Berg <sb224sc@student.lnu.se>
2024-10-16 21:55:45 +0200

    completed task3 lab2 2dt903

--- y3/code/2dt903/labs/lab2/main.c
+++ y3/code/2dt903/labs/lab2/main.c
@@ -1,25 +1,31 @@
 #include <stdio.h>
+#include <dht.h>
 #include "pico/stdlib.h"
 #include "FreeRTOS.h"
 #include "task.h"
-#include "DHT.h"
 
-define BTN_PIN 0;
-define LED_PIN 1;
-define DHT_PIN 16;
+#define BTN_PIN 0
+#define LED_PIN 1
+#define DHT_PIN 16
 
-DHT dht;
+static const dht_model_t DHT_MODEL = DHT11;
+dht_t dht;
 
 void temp_task(void *pvParameters) {
     for (;;) {
-        dht.measure();
-        float temp = dht.getTemperatureFloat();
-        float hum = dht.getHumidityFloat();
+        dht_start_measurement(&dht);
+        
+        float temp;
+        float hum;
+        dht_result_t res = dht_finish_measurement_blocking(&dht, &hum, &temp);
 
-        if (isnan(temp) || isnan(hum)) {
-            printf("Failed to read DHT sensor values!\n");
-        } else {
+        if (res == DHT_RESULT_OK) {
             printf("Temperature: %.1f °C, Humidity: %.1f%%\n", temp, hum);
+        } else if (res == DHT_RESULT_TIMEOUT){
+            printf("DHT sensor not responding. Please check your wiring.\n");
+        } else {
+            assert(result == DHT_RESULT_BAD_CHECKSUM);
+            puts("Bad checksum");
         }
 
         vTaskDelay(pdMS_TO_TICKS(2000));
@@ -30,8 +36,9 @@ void btn_task(void *pvParameters) {
     bool ledState = false;
 
     while (1) {
+        printf("Button state: %d\n", gpio_get(BTN_PIN));
         if (gpio_get(BTN_PIN)) {
-            ledState != ledState;
+            ledState = !ledState;
             gpio_put(LED_PIN, ledState);
             vTaskDelay(pdMS_TO_TICKS(500));
         }
@@ -45,12 +52,11 @@ int main() {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    gpio_init(BUTTON_PIN);
-    gpio_set_dir(BUTTON_PIN, GPIO_IN);
-    gpio_pull_up(BUTTON_PIN);
+    gpio_init(BTN_PIN);
+    gpio_set_dir(BTN_PIN, GPIO_IN);
+    gpio_pull_up(BTN_PIN);
 
-    gpio_init(DHT_PIN);
-    dht = DHT11(DHT_PIN);
+    dht_init(&dht, DHT_MODEL, pio0, DHT_PIN, true);
 
     xTaskCreate(temp_task, "tempTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
     xTaskCreate(btn_task, "btnTask", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
