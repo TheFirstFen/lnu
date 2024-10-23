fb7ffde020c812172728c79030c6c3c5fafd03f5
Samuel Berg <sb224sc@student.lnu.se>
2024-10-23 11:02:30 +0200

    1ma916 lab progress

--- y3/matlab/1ma916/labs/lab1/Task8.m
+++ y3/matlab/1ma916/labs/lab1/Task8.m
@@ -1,24 +1,36 @@
-% Parameters for the sphere
 clc
 clear
+[x, y, z] = sphere();
 r = 6;
-center = [-2, 1, 3];
+
+x = 6 * x - 2;
+y = 6 * y + 1;
+z = 6 * z + 3;
+
+figure;
+surf(x, y, z);
+xlabel('x');
+ylabel('y');
+zlabel('z');
+title('Made using sphere function');
+axis equal;
+grid on;
+view(35, 35);
 
 theta = linspace(0, 2*pi, 100);
 phi = linspace(0, pi, 100);
-
 [Theta, Phi] = meshgrid(theta, phi);
 
-X = r * sin(Phi) .* cos(Theta) + center(1);
-Y = r * sin(Phi) .* sin(Theta) + center(2);
-Z = r * cos(Phi) + center(3);
+X = r * sin(Phi) .* cos(Theta) - 2;
+Y = r * sin(Phi) .* sin(Theta) + 1;
+Z = r * cos(Phi) + 3;
 
 figure;
-surf(X, Y, Z, 'FaceColor', 'b', 'EdgeColor', 'none');
+surf(X, Y, Z);
 axis equal;
 grid on;
 xlabel('X-axis');
 ylabel('Y-axis');
 zlabel('Z-axis');
-title('Sphere with Radius 6 Centered at (-2, 1, 3)');
+title('Made without using sphere function');
 view(35, 35);
