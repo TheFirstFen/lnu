1f852717c20287e51d7c0582933853eb8ef00fcc
william <william.schroder11@gmail.com>
2024-11-12 16:07:32 +0100

    linear regression

--- yoda/1DV505/ws222gx_assign1/3-sum/3_sum.py
+++ yoda/1DV505/ws222gx_assign1/3-sum/3_sum.py
@@ -3,8 +3,10 @@ import matplotlib.pyplot as plt
 from algorithms.bf import brute_force
 from algorithms.two_pointer import two_pointer
 from algorithms.caching import caching
+from utils.linear_regression import find_m_k
 
-def benchmark_function(test_func, target=50, min_size=0, max_size=300,
+
+def benchmark_function(test_func, target=50, min_size=0, max_size=100,
                        step=10, trials=5):
 
     size_lst = list(range(min_size, max_size + 1, step))
@@ -31,17 +33,25 @@ def benchmark_function(test_func, target=50, min_size=0, max_size=300,
 labels = ["Brute force", "Two pointer", "Caching"]
 
 avg_times_bf, size_lst = benchmark_function(brute_force)
-avg_times_tP, size_lst = benchmark_function(two_pointer)
-avg_times_ch, size_lst = benchmark_function(caching)
+# avg_times_tP, size_lst = benchmark_function(two_pointer)
+# avg_times_ch, size_lst = benchmark_function(caching)
 
 print("Brute force:", avg_times_bf[-1])
-print("Two pointer:", avg_times_tP[-1])
-print("Caching:", avg_times_ch[-1])
+# print("Two pointer:", avg_times_tP[-1])
+# print("Caching:", avg_times_ch[-1])
 
+k, m = find_m_k(size_lst, avg_times_bf)
+
+linear_lst = []
+for i in range(len(size_lst)):
+    regression = k*i + m
+    linear_lst.append(regression)
+print(k, m)
 
 plt.plot(size_lst, avg_times_bf, "-+r", label=labels[0])
-plt.plot(size_lst, avg_times_tP, "-+b", label=labels[1])
-plt.plot(size_lst, avg_times_ch, "-+y", label=labels[2])
+plt.plot(size_lst, linear_lst, "-+b", label=f"k = {k}\nm = {m}")
+# plt.plot(size_lst, avg_times_tP, "-+b", label=labels[1])
+# plt.plot(size_lst, avg_times_ch, "-+y", label=labels[2])
 
 plt.xlabel("List Size")
 plt.ylabel("Average Execution Time (s)")
---
+++ yoda/1DV505/ws222gx_assign1/3-sum/utils/linear_regression.py
@@ -0,0 +1,30 @@
+def Sx_y(x, y):
+    return sum(x), sum(y)
+
+
+def Sxx(x):
+    summa = 0
+    for i in x:
+        summa += i**2
+    return summa
+
+
+def Sxy(x, y):
+    summa = 0
+    print(len(x), len(y))
+    for i, j in zip(x, y):
+        summa += (i*j)
+    return summa
+
+def find_m_k(x, y):
+    xx = Sxx(x)
+    xy = Sxy(x, y)
+    s_x, s_y = Sx_y(x, y)
+    n = len(x)
+
+    numerator = (n*xx) - (s_x*s_x)
+
+    k = (n*xy) - (s_x * s_y) / numerator
+    m = (xx - (s_x * xy)) / numerator
+
+    return k, m
