c8f32d9ebaf821f615fc65ac5375814676334552
Samuel Berg <sb224sc@student.lnu.se>
2024-11-14 11:19:00 +0100

    fix

--- fen/y3/code/2dt904/labs/lab1/main.py
+++ fen/y3/code/2dt904/labs/lab1/main.py
@@ -79,26 +79,5 @@ class Point:
         glDeleteVertexArrays(1, [self.vao])
         glDeleteBuffers(1, [self.vbo])
 
-class Material:
-    def __init__(self, filePath):
-        self.texture = glGenTextures(1)
-        glBindTexture(GL_TEXTURE_2D, self.texture)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
-        image = pg.image.load(filePath).convert()
-        image_width, image_height = image.get_rect().size
-        image_data = pg.image.tostring(image, 'RGBA')
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data)
-        glGenerateMipmap(GL_TEXTURE_2D)
-
-    def use(self):
-        glActiveTexture(GL_TEXTURE0)
-        glBindTexture(GL_TEXTURE_2D, self.texture)
-
-    def destroy(self):
-        glDeleteTextures(1, (self.texture,))
-
 if __name__ == '__main__':
     myApp = App()
