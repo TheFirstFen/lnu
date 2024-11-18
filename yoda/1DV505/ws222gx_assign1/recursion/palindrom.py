b4b70a5c0bff6a6eecdbc8a121d4a1b51a075ed4
William Skröder <william.schroder11@gmail.com>
2024-11-18 19:36:35 +0100

    less complex palindrome soulution

--- yoda/1DV505/ws222gx_assign1/recursion/palindrom.py
+++ yoda/1DV505/ws222gx_assign1/recursion/palindrom.py
@@ -1,31 +1,26 @@
-def is_palindrome(s, first, last):
-    if first >= last:
-        return True
-
-    if s[first] == s[last]:
-        first += 1
-        last -= 1
-        return is_palindrome(s, first, last)
-    return False
 
 
 def check_palindrome(s):
     if len(s) <= 1:
         return True
 
-    first, last = 0, len(s)-1
-    return is_palindrome(s, first, last)
+    if s[0] == s[-1]:
+        return check_palindrome(s[1:-1])
+
+    return False
 
 
-true_cases = ["", " x", "annna", "madam", "abcdefedcba", "yyyyyyyyy"]
+true_cases = ["", "x", "annna", "madam", "abcdefedcba", "yyyyyyyyy"]
 false_cases = ["programming", "university", "microbiology",
                "counterproductive", "exaggeration"]
 
 
+print("Expected results to be all True")
 for i in true_cases:
     result = check_palindrome(i)
     print(result)
 
+print("\nExpected results to be all False")
 for j in false_cases:
     result = check_palindrome(j)
     print(result)
