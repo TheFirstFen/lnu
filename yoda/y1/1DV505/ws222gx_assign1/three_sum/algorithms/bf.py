1b4103fac3dce9dc95ee61bd9686a3132f5b7050
William Skröder <william.schroder11@gmail.com>
2024-11-21 12:36:02 +0100

    .

--- yoda/y1/1DV505/ws222gx_assign1/three_sum/algorithms/bf.py
+++ yoda/y1/1DV505/ws222gx_assign1/three_sum/algorithms/bf.py
@@ -5,10 +5,14 @@ def brute_force(lst, target):
         for j in range(i+1, len(lst)-1):
             for k in range(j+1, len(lst)):
                 v1, v2, v3 = lst[i], lst[j], lst[k]
-                needed = (v1 + v2 + v3) - target
-                if i == j or k == i or k == j:
-                    continue
-                if needed == 0:
+                needed = v1 + v2 + v3
+                if needed == target:
+                    if v2 < v1:
+                        v1, v2 = v2, v1
+                    if v3 < v1:
+                        v1, v3 = v3, v1
+                    if v3 < v2:
+                        v2, v3 = v3, v2
                     unique_pairs.add((v1, v2, v3))
 
     return list(unique_pairs)
