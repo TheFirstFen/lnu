2730ec8331c405265556ed290e3f86160a08cfe5
Emil Ulvagården <emil@ulvagarden.st>
2024-11-28 13:03:04 +0100

    ...

--- ulv/year_3/2DT904/Lab3/1-transformations.py
+++ ulv/year_3/2DT904/Lab3/1-transformations.py
@@ -3,8 +3,10 @@ from matrix import Matrix
 from app import run, readFile
 from glslprogram import Program
 from meshes import setupTriangle
+from math import pi
 import os 
 os.environ["SDL_VIDEO_X11_FORCE_EGL"] = "1"
+
 
 vsCode = readFile('1-transformations-vs.glsl')
 fsCode = readFile('varying-color-fs.glsl')
@@ -19,10 +21,14 @@ def init():
 
     drawCount = setupTriangle(program.programId)
 
-    mModel = Matrix.makeIdentity()
+    mModel = Matrix.makeTranslation(0.5, 0.5, -40.0) @ Matrix.makeScale(0.5) @ Matrix.makeRotationZ(pi / 2.444)
     program.setUniformMat4('mModel', mModel)
 
-    mProjView = Matrix.makeIdentity()
+    projection = Matrix.makePerspective()
+    mProjView = projection #Matrix.makeIdentity()
+
+    invcameraPos = Matrix.makeTranslation(0, 0, -10)
+    mProjView = projection @ invcameraPos
     program.setUniformMat4('mProjView', mProjView)
 
 
@@ -30,4 +36,4 @@ def update(dt, time):
     glDrawArrays(GL_TRIANGLES, 0, drawCount)
 
 
-run("2DT904 - Transformations", init=init, update=update, screenSize=[512,512])
+run("2DT904 - Transformations", init=init, update=update, screenSize=[800,800])
