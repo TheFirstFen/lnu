cbba103550f31cd15269697dfdfd644a57663d27
Samuel Berg <sb224sc@student.lnu.se>
2025-01-28 11:17:07 +0100

    ...

--- fen/y3/code/2dt906/assign/a1/sub_transpos_cipher.py
+++ fen/y3/code/2dt906/assign/a1/sub_transpos_cipher.py
@@ -1,42 +1,70 @@
+
+alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def create_alphabet(key):
+    new_alphabet = ""
+    for c in key:
+        if c not in new_alphabet:
+            new_alphabet += c
+
+    new_alphabet = new_alphabet.replace(" ", "")
+
+    for c in alphabet:
+        if c not in new_alphabet:
+            new_alphabet += c
+
+    return new_alphabet
+
+
 def substitution_encrypt(text, key):
-    encrypted_text = ""
+    new_alphabet = create_alphabet(key)
+    enc_text = ""
     for char in text:
         if char.isalpha():
-            shift = ord(key) % 256
-            encrypted_char = chr((ord(char) + shift) % 256)
-            encrypted_text += encrypted_char
+            enc_text += new_alphabet[alphabet.index(char)]
         else:
-            encrypted_text += char
-    return encrypted_text
+            enc_text += char
+
+    return enc_text
+
 
 def substitution_decrypt(text, key):
-    decrypted_text = ""
+    new_alphabet = create_alphabet(key)
+    dec_text = ""
     for char in text:
         if char.isalpha():
-            shift = ord(key) % 256
-            decrypted_char = chr((ord(char) - shift) % 256)
-            decrypted_text += decrypted_char
+            dec_text += alphabet[new_alphabet.index(char)]
         else:
-            decrypted_text += char
-    return decrypted_text
+            dec_text += char
+
+    return dec_text
+
 
 def transposition_encrypt(text, key):
-    key_length = len(str(key))
-    encrypted_text = [''] * key_length
+    key_len = len(str(key))
+
+    enc_text = [''] * key_len
     for i, char in enumerate(text):
-        encrypted_text[i % key_length] += char
-    return ''.join(encrypted_text)
+        enc_text[i % key_len] += char
+
+    return ''.join(enc_text)
+
 
 def transposition_decrypt(text, key):
-    key_length = len(str(key))
-    decrypted_text = [''] * ((len(text) // key_length) + 1)
+    key_len = len(str(key))
+
+    dec_text = [''] * ((len(text) // key_len) + 1)
     for i, char in enumerate(text):
-        decrypted_text[i % len(decrypted_text)] += char
-    return ''.join(decrypted_text)
+        dec_text[i % len(dec_text)] += char
+
+    return ''.join(dec_text)
+
 
 def main():
-    operation = input("Do you want to encrypt (E) or decrypt (D)? ").upper()
-    method = input("Do you want to use substitution (S) or transposition (T)? ").upper()
+    op = input("Do you want to encrypt (E) or decrypt (D)? ").upper()
+    method = input(
+        "Do you want to use substitution (S) or transposition (T)? ").upper()
     key = input("Input the secret key: ")
     filename = input("Input the name of the file you want to process: ")
 
@@ -44,21 +72,33 @@ def main():
         text = file.read()
 
     if method == 'S':
-        if operation == 'E':
+        if op == 'E':
             processed_text = substitution_encrypt(text, key)
-        else:
+        elif op == 'D':
             processed_text = substitution_decrypt(text, key)
-    elif method == 'T':
-        if operation == 'E':
-            processed_text = transposition_encrypt(text, key)
         else:
+            print("Invalid operation. Please choose either E or D.")
+            return
+    elif method == 'T':
+        if op == 'E':
+            processed_text = transposition_encrypt(text, key)
+        elif op == 'D':
             processed_text = transposition_decrypt(text, key)
+        else:
+            print("Invalid operation. Please choose either E or D.")
+            return
+    else:
+        print("Invalid method. Please choose either S or T.")
+        return
 
-    output_filename = filename.split('.')[0] + '_enc.txt' if operation == 'E' else filename.split('.')[0] + '_dec.txt'
+    output_filename = filename.split(
+        '.')[0] + '_enc.txt' if op == 'E' else filename.split('.')[0] + '_dec.txt'
     with open(output_filename, 'w') as file:
         file.write(processed_text)
 
-    print(f"The file has been processed and the results have been saved in the file {output_filename}")
+    print(f"The file has been processed and the results have been saved in the file {
+          output_filename}")
+
 
 if __name__ == "__main__":
     main()
