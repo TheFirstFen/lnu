f5f145e623be777437bb55a5e9fddafcdd5177e3
Samuel Berg <sb224sc@student.lnu.se>
2025-01-28 21:23:40 +0100

    renamed functions

--- fen/y3/code/2dt906/assign/a1/task6a.py
+++ fen/y3/code/2dt906/assign/a1/task6a.py
@@ -1,11 +1,11 @@
-def hash(line):
+def old_hash(line):  # From year 1 1DT901 project
     hash_value = 0
     for char in line:
         hash_value += ord(char)
     return (217 + hash_value) % 256
 
 
-def improved_hash(line):
+def hash(line):
     # Starting with a prime number
     hash_value = 5381
     for i, char in enumerate(line):
