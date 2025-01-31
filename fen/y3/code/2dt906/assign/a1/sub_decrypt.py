60d93d646ec81a9721106c7770fed7f166286837
Samuel Berg <sb224sc@student.lnu.se>
2025-01-31 09:30:58 +0100

    Completed sub_decrypt; Added others encrypted files; Preped tanspos_dectypt;

--- fen/y3/code/2dt906/assign/a1/sub_decrypt.py
+++ fen/y3/code/2dt906/assign/a1/sub_decrypt.py
@@ -1,4 +1,33 @@
 # Author: Samuel Berg
-# TODO Date: 2025-0X-XX
+# Date: 2025-01-31
 # For: Task 5 in Assignment 1 in course 2DT906 at LNU
-# TODO Completion: WiP!!
+# Completion: Done!
+
+# For md223rb's files
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+# Modify for your shifted alphabet
+shifted_alphabet = "wxyzabcdefghijklmnopqrstuv"
+
+
+def substitution_decrypt(text: str) -> str:
+    """
+    Decrypts text that was encrypted using substitution cipher
+    Args:
+        text (str): String to decrypt
+        key (str): Encryption key
+    Returns:
+        str: Decrypted string
+    """
+    trans_table = str.maketrans(alphabet, shifted_alphabet)
+
+    return text.translate(trans_table)
+
+
+def main():
+    with open('./data/task5/md223rb_sub.txt', 'r') as file:
+        text = file.read()
+
+    processed_text = substitution_decrypt(text)
+
+    with open('./data/task5/md223rb_sub_dec.txt', 'w') as file:
+        file.write(processed_text)
--- fen/y3/code/2dt906/assign/a1/transpos_decrypt.py
+++ fen/y3/code/2dt906/assign/a1/transpos_decrypt.py
@@ -2,3 +2,28 @@
 # TODO Date: 2025-0X-XX
 # For: Task 5 in Assignment 1 in course 2DT906 at LNU
 # TODO Completion: WiP!!
+
+
+def transposition_decrypt(text: str) -> str:
+    """
+    Decrypts text that was encrypted using transposition cipher
+    Args:
+        text (str): String to decrypt
+    Returns:
+        str: Decrypted string
+    """
+    pass
+
+
+def main():
+    with open('./data/task5/md223rb_tran.txt', 'r') as file:
+        text = file.read()
+
+    processed_text = transposition_decrypt(text)
+
+    with open('./data/task5/md223rb_tran_dec.txt', 'w') as file:
+        file.write(processed_text)
+
+
+if __name__ == "__main__":
+    main()
